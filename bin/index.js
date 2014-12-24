0110f194f5c21c84fdbd8c69ff8769b06770117d
Jason Jarrett <jason@elegantcode.com>
2014-12-24 12:40:22 -0800

    first attempt at using a local config to override global config

--- bin/index.js
+++ bin/index.js
@@ -3,13 +3,19 @@
 
 var spawn = require('child_process').spawn;
 var path = require('path');
+var _ = require('lodash');
+var Liftoff = require('liftoff');
 var parseArgs = require('../skypeit').parseArgs;
 var cleanArgs = process.argv.map(function (item) {
   return (item || '').toLowerCase();
 });
 
+function getUserHome() {
+  return process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'];
+}
+
+var globalConfigFileName = '.skypeitrc';
 var debug = cleanArgs.indexOf('--debug') >= 0 || cleanArgs.indexOf('--verbose') >= 0;
-var config = require('../lib/config.js')(debug);
 
 var dlog = function () {
   if(debug) {
@@ -36,81 +42,109 @@ var printHelpMessage = function() {
   console.log(output);
 };
 
+
+var Skypeit = new Liftoff({
+  processTitle: 'SkypeIt',
+  moduleName: 'skypeit',
+  configName: globalConfigFileName,
+  extensions:{
+    '': null
+  }
+});
+
 var argv = require('minimist')(process.argv.slice(2));
 
-dlog('args: ', argv);
+var invoke = function (env) {
 
-if(argv.help) {
-  printHelpMessage();
-  return;
-}
+  dlog('Environment is:', env);
+  dlog('Cli options are:', argv);
+  dlog('Liftoff config is:', this);
 
-if(argv.version) {
-  console.log(getVersion());
-  return;
-}
-
-if(argv.completion) {
-
-  if(argv.completion === true) {
-    console.log(config.listAlias.join('\n'));
+  if(argv.help) {
+    printHelpMessage();
     return;
   }
 
-  require('../lib/completion')(argv.completion);
-  return;
-}
+  if(argv.version) {
+    console.log(getVersion());
+    return;
+  }
 
-var aliasNumber;
-var alias = argv._[0];
-var result;
-dlog("config.alias: ", config.alias);
-if(alias && config.alias[alias]) {
-  dlog('found alias for: ' + alias);
-  var aliasValue = config.alias[alias];
-  dlog('alias:', aliasValue);
-  aliasNumber = aliasValue.number;
-  result = parseArgs(aliasNumber.split(' '), debug);
-} else {
-  dlog('no alias found');
-  result = parseArgs(argv._, debug);
-}
+  var globalConfigPath = path.join(getUserHome(), globalConfigFileName);
+  var localConfigPath = path.join(env.configBase, globalConfigFileName);
+  dlog('globalConfigPath:', globalConfigPath);
+  dlog('localConfigPath:', localConfigPath);
+  var config = require('../lib/config.js')(debug, globalConfigPath, localConfigPath);
 
-if(!result) {
-  printHelpMessage();
-  var chalk = require('chalk');
-  console.log(chalk.red("invalid input... Consider using the --debug parameter to help diagnose issues."));
-  return;
-}
+  if(argv.completion) {
 
-dlog('process.argv:', process.argv);
-dlog('debug:', debug);
-dlog('parseArgs: ', result);
+    if(argv.completion === true) {
+      console.log(config.listAlias.join('\n'));
+      return;
+    }
 
-var appleScriptArgs = [ path.join(__dirname, './skypeItAppleScript.scpt'), result.num, result.ext];
+    require('../lib/completion')(argv.completion);
+    return;
+  }
 
-var appleScriptCmd = 'osascript';
+  var aliasNumber;
+  var alias = argv._[0];
+  var result;
+  dlog("config.alias: ", config.alias);
+  if(alias && config.alias[alias]) {
+    dlog('found alias for: ' + alias);
+    var aliasValue = config.alias[alias];
+    dlog('alias:', aliasValue);
+    aliasNumber = aliasValue.number;
+    result = parseArgs(aliasNumber.split(' '), debug);
+  } else {
+    dlog('no alias found');
+    result = parseArgs(argv._, debug);
+  }
 
-dlog(appleScriptCmd + ' ' + appleScriptArgs.join(' '));
+  if(!result) {
+    printHelpMessage();
+    var chalk = require('chalk');
+    console.log(chalk.red("invalid input... Consider using the --debug parameter to help diagnose issues."));
+    return;
+  }
 
-if(debug) {
-  var cmd = spawn(appleScriptCmd, appleScriptArgs);
+  dlog('process.argv:', process.argv);
+  dlog('debug:', debug);
+  dlog('parseArgs: ', result);
 
-  cmd.stdout.on('data', function (data) {
-    console.log('stdout: ' + data);
-  });
+  var appleScriptArgs = [ path.join(__dirname, './skypeItAppleScript.scpt'), result.num, result.ext];
 
-  cmd.stderr.on('data', function (data) {
-    console.error('stderr: ' + data);
-  });
+  var appleScriptCmd = 'osascript';
 
-  cmd.on('close', function (code) {
-    console.log('child process exited with code ' + code);
-  });
-} else {
-  var cmd = spawn(appleScriptCmd, appleScriptArgs, {
-    detached: true,
-    stdio: ['ignore', 'ignore', 'ignore']
-  });
-  cmd.unref();
-}
+  dlog(appleScriptCmd + ' ' + appleScriptArgs.join(' '));
+
+  if(debug) {
+    var cmd = spawn(appleScriptCmd, appleScriptArgs);
+
+    cmd.stdout.on('data', function (data) {
+      console.log('stdout: ' + data);
+    });
+
+    cmd.stderr.on('data', function (data) {
+      console.error('stderr: ' + data);
+    });
+
+    cmd.on('close', function (code) {
+      console.log('child process exited with code ' + code);
+    });
+  } else {
+    var cmd = spawn(appleScriptCmd, appleScriptArgs, {
+      detached: true,
+      stdio: ['ignore', 'ignore', 'ignore']
+    });
+    cmd.unref();
+  }
+
+};
+Skypeit.launch({
+  cwd: argv.cwd,
+  configPath: argv.myappfile,
+  require: argv.require,
+  completion: argv.completion
+}, invoke);
